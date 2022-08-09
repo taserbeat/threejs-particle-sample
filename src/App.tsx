eed8bc5479f7ddf51ac1fb266364fb22d41ac02f
taserbeat <taserbeat@gmail.com>
2022-08-09 20:18:08 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
+
+import "./App.css";
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+// GUI
+const gui = new GUI();
+
+// シーン
+const scene = new THREE.Scene();
+
+// カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+
+camera.position.set(1, 1, 2);
+scene.add(camera);
+
+// レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+/**
+ * テクスチャ設定
+ */
+
+/**
+ * パーティクルを作成する
+ */
+
+// マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+// クロック
+const clock = new THREE.Clock();
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+function App() {
+  const refDiv = useRef<HTMLDivElement>(null);
+
+  const onBrowserResize = () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    return;
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onBrowserResize);
+
+    refDiv.current?.appendChild(renderer.domElement);
+
+    const updateRender = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      controls.update();
+
+      // レンダリング
+      renderer.render(scene, camera);
+      requestAnimationFrame(updateRender);
+    };
+
+    updateRender();
+
+    return () => {
+      refDiv.current?.removeChild(renderer.domElement);
+      removeEventListener("mousemove", onBrowserResize);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="scene" ref={refDiv}></div>
+    </div>
+  );
+}
+
+export default App;
